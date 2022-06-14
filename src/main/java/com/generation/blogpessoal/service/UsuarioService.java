5d9e0365e9637cc31f53c282280d05c7732ea54a
unknown <andre.dev.oliveira@gmail.com>
2022-06-14 18:05:43 -0300

    “Deploy”

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -3,16 +3,16 @@ package com.generation.blogpessoal.service;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import com.generation.blogpessoal.model.Usuario;
-import com.generation.blogpessoal.model.UsuarioLogin;
-import com.generation.blogpessoal.repository.UsuarioRepository;
 
 /**
  *  A Classe UsuarioService implementa as regras de negócio do Recurso Usuario.
@@ -55,6 +55,7 @@ public class UsuarioService {
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
 
 		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			return Optional.empty();
 		
 		/**
 		 * Se o Usuário não existir no Banco de Dados, a senha será criptografada
@@ -88,15 +89,35 @@ public class UsuarioService {
 		
 		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
 			
-			Optional<Usuario> buscausuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+			/**
+			 * Cria um Objeto Optional com o resultado do método findById
+			 */
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
 			
-			if (buscausuario.isPresent() && buscausuario.get().getId() != usuario.getId())
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"usuario ja existe",null);
-			
+			/**
+			 * Se o Usuário existir no Banco de dados e o Id do Usuário encontrado no Banco for 
+			 * diferente do usuário do Id do Usuário enviado na requisição, a Atualização dos 
+			 * dados do Usuário não pode ser realizada.
+			 */
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			/**
+		 	* Se o Usuário existir no Banco de Dados e o Id for o mesmo, a senha será criptografada
+		 	* através do Método criptografarSenha.
+		 	*/
 			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			/**
+		 	* Assim como na Expressão Lambda, o resultado do método save será retornado dentro
+		 	* de um Optional, com o Usuario persistido no Banco de Dados ou um Optional vazio,
+			* caso aconteça algum erro.
+			* 
+			* ofNullable​ -> Se um valor estiver presente, retorna um Optional com o valor, 
+			* caso contrário, retorna um Optional vazio.
+		 	*/
 			return Optional.ofNullable(usuarioRepository.save(usuario));
-			
-			
 			
 		}
 		
