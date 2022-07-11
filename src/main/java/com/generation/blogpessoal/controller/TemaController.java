e22e4829d2b418c2c061e91d990531ed3bc56b14
unknown <andre.dev.oliveira@gmail.com>
2022-07-11 09:50:52 -0300

    erro minimo

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -54,8 +54,7 @@ public class TemaController
 	}
 	
 	@PostMapping
-	public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema)
-	{
+	public ResponseEntity<Tema> postTema (@Valid @RequestBody Tema tema){
 		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
 	
